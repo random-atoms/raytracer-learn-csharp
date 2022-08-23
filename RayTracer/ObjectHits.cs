304f2bd8bcdfa51a26f324c8e9275cda6aa87eda
random-atoms <110063771+random-atoms@users.noreply.github.com>
2022-08-22 21:18:50 -0400

    Attempt at object list - does not compile

--- RayTracer/ObjectHits.cs
+++ RayTracer/ObjectHits.cs
@@ -7,6 +7,33 @@ using System.Threading.Tasks;
 
 namespace RayTracer
 {
+    public struct HitRecord
+    {
+        public Vector3 Point {  get; set; }
+        public Vector3 Normal { get; set; }
+        public float T {  get; set; }
+
+        public bool FrontFace;
+    
+        public void SetFaceNormal (Ray ray, Vector3 outwardNormal)
+        {
+            FrontFace = Vector3.Dot(ray.Direction, outwardNormal) < 0;
+            if (FrontFace)
+            {
+                Normal = outwardNormal;
+            }
+            else
+            {
+                Normal = -outwardNormal;
+            }
+        }
+
+    }
+
+    public interface Hittable
+    {
+        public bool Hit(Ray ray, float tMin, float tMax, out HitRecord hR);
+    }
     public static class ObjectHits
     {
         public static float SphereHit(Vector3 center, float radius, Ray ray)
@@ -24,6 +51,63 @@ namespace RayTracer
             {
                 return (-halfB - (float)Math.Sqrt(discriminant)) / a;
             }
+        }
+    }
+    public class Sphere : Hittable
+    {
+        public Vector3 Center { get; }
+        public float Radius { get;  }
+        Sphere(Vector3 center, float radius)
+        {
+            this.Center = center;
+            this.Radius = radius;
+        }
+        public bool Hit(Ray ray, float tMin, float tMax, out HitRecord hR)
+        {
+            hR = new HitRecord();
+
+            Vector3 oc = ray.Origin - Center;
+            var a = ray.Direction.LengthSquared();
+            var halfB = Vector3.Dot(oc, ray.Direction);
+            var c = oc.LengthSquared() - Radius * Radius;
+            var discriminant = halfB * halfB - a * c;
+            if (discriminant < 0)
+            {
+                return false;
+            }
+            var sqrtD = (float)Math.Sqrt(discriminant);
+
+
+            // Find the nearest root that lies in the acceptable range.
+            var root = (-halfB - sqrtD) / a;
+            if (root < tMin || tMax < root)
+            {
+                root = (-halfB + sqrtD) / a;
+                if (root < tMin || tMax < root)
+                    return false;
+            }
+
+            hR.T = root;
+            hR.Point = ray.At(hR.T);
+            Vector3 outwardNormal = (hR.Point - Center) / Radius;
+            hR.SetFaceNormal(ray, outwardNormal);
+
+            return true;
+   
+        }
+    }
+
+    public class HittableObjects : Hittable, IEquatable<Hittable>
+    {
+        public bool Hit(Ray ray, float tMin, float tMax, out HitRecord hR)
+        {
+            hR = new HitRecord();
+            return false;
+        }
+        public override bool Equals(Hittable other)
+        {
+            if (other == null) return false;
+            else return true;
         }
     }
 }
